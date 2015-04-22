7010a1087137e80c5fe933a3276889a5d0ebb189
jangorecki <j.gorecki@wit.edu.pl>
2015-04-22 01:41:25 +0100

    valid test.data.table, still manual

--- tests/manual/test-test.data.table.R
+++ tests/manual/test-test.data.table.R
@@ -1,13 +1,20 @@
 context("test.data.table")
-# moved to manual as not needed to test it every time, also good to resolve #2 first
 
 test_that("test dtq and test.data.table", {
   
+  test.dt <- data.table::test.data.table
+  environment(test.dt) <- .BaseNamespaceEnv
+  
   dtl(purge = TRUE)
-  DT <- data.table(a = 1:10, b = letters[1:5])
-  DT[]
+  DT <- data.table(a = 1, b = letters[1])
+  f <- function(x) x[]
+  environment(f) <- .BaseNamespaceEnv
+  f(DT)
   
   expect_identical(dtq.log$length(), 1L, info="test.data.table confirm logging active")
-  expect_null(suppressMessages(suppressMessages(test.data.table())), info="test.data.table without errors")
+  expect_identical(dtq.log$log[[1L]]$env, "base", info="test.data.table confirm logging active env")
+  expect_null(suppressMessages(suppressMessages(test.dt())), info="test.data.table without errors")
+  rm(test.dt)
+  expect_true(dtq.log$length() > 7000L, info="test.data.table confirm more logs than 7e3")
   
 })
